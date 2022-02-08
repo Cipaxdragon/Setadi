b5c3f00d1bd62d5b3287f05ecf0c6faec4b77fcf
Asep <bikingimil@gmail.com>
2022-02-08 13:22:01 +0800

    menambahkan syntak javascript

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-function berubah(){
-    document.getElementById("x").innerHTML = "Y";
+//alert
+/* 
+alert("Ini Cuma WebSait Untuk Latihan Moon Maap Kalo Jeleq");
+alert("By Asep");
+ */
+//variabel
+var num = 0;
+
+//function
+
+
+function tambah(){
+    num = num + 1;
+    document.getElementById("angka").innerHTML = num;
+    
+    if(num == 10){
+        num = 100;
+        document.getElementById("angka").innerHTML = num;
+    
+    }
 }
-
-function name() {
-    document.getElementById("")
-}
-
-
-
